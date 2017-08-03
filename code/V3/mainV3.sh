bd500b6bdd8250810af9c05af9377b8ec4f74934
ZhunLiang <594333210@qq.com>
2017-08-03 11:06:52 +0800

    change mainV3.sh

--- code/V3/mainV3.sh
+++ code/V3/mainV3.sh
@@ -22,8 +22,9 @@ $Kmax=1.1; #the maxmiun density of channel ion compare with bulk
 $Kmin=1.02; #the mimnium density of channel ion compare with bulk
 
 sub GetMSD{
-    @TEMP = split/\s+/,`grep $_[0] MSD_out.dat`;
-    $TEMP_num = @TEMP;
+    my @TEMP = split/\s+/,`grep $_[0] MSD_out.dat`;
+    my $TEMP_num = @TEMP;
+    my @Out;
     for($i=1;$i<$TEMP_num;$i=$i+1){
         $j=$i-1;
         @Out[$j] = @TEMP[$i];
@@ -31,10 +32,25 @@ sub GetMSD{
     return @Out;
 }
 
-system "python GetMSDpara.py";
+sub GetTopNum{
+  my $count = 0;
+  my $temp,@temp2,@Out;
+  for(my $i=0;$i<$TotalNum;$i+=1){
+    $temp = `grep -w @MoleName[$i] $_[0]`;
+    if($temp){
+      @temp2 = split/\s+/,$temp;
+      @Out[$count] = @temp2[1];
+      $count += 1;
+    }
+  }
+  return @Out;
+}
+
+system "/opt/python/bin/python GetMSDpara.py";
 @MoleName = GetMSD("NAME");
 @MoleMass = GetMSD("MASS");
 @MoleNum = GetMSD("NUM");
+$TotalNum = @MoleName;
 
 #
 @GroXYZ= split/\s+/,`tail -1 $file`; #GroXYZ[1] [2] [3] is X, Y, Z
@@ -59,65 +75,67 @@ system "editconf -f temp_out.gro -scale @ScaleSize[0] @ScaleSize[1] @ScaleSize[2
 @NewGroXYZ=split/\s+/, `tail -1 tempLeft.gro`; #Get the scaled box size: X, Y, Z
 system "rm -f temp_out.gro";
 #Get the scaled gro, the x,y,z is %.3f format
-system "PYTHON ~/bin/GetZmax.py -i tempLeft.gro > LPyOut";
+system "/opt/python/bin/python GetZmax.py -i tempLeft.gro > LPyOut";
 @ZLMaxMin= split/\s+/,`cat LPyOut`; #Get the LeftGro Zmax[0] and Zmin[1]
 system "editconf -f tempLeft.gro -scale 1 1 -1 -o tempRight.gro";
-system "PYTHON ~/bin/GetZmax.py -i tempRight.gro > RPyOut";
+system "/opt/python/bin/python GetZmax.py -i tempRight.gro > RPyOut";
 @ZRMaxMin=split/\s+/,`cat RPyOut`; #Get the RightGro Zmax[0] and Zmin[1]
 $Ztrans=@ZLMaxMin[0]-@ZRMaxMin[1]+$ChannelLong; 
 system "editconf -f tempRight.gro -translate 0 0 $Ztrans -o tempRightT.gro";
 #Translate the left gro as right gro
 @NewGroXYZ[3]=$TotalZLong;
-system "PYTHON ~/bin/CombineGro.py -l tempLeft.gro -r tempRightT.gro -n $channel_name -x @NewGroXYZ[1] -y @NewGroXYZ[2] -z @NewGroXYZ[3] -o output_temp1.gro";
+system "/opt/python/bin/python CombineGro.py -l tempLeft.gro -r tempRightT.gro -n $channel_name -x @NewGroXYZ[1] -y @NewGroXYZ[2] -z @NewGroXYZ[3] -o output_temp1.gro";
 #Combine these two gro as one total gro
 system "editconf -f output_temp1.gro -c -o $channel_grofile";
-@SingleWallNum=split/\s+/,`grep -w @MoleName[0] $channel_topfile`;
-$NewWallNum=@SingleWallNum[1]*@GroNum[0]*@GroNum[1]*@GroNum[2]*2;
+@SingleWallNum=GetTopNum($channel_topfile);
+print "SingleWallNum: @SingleWallNum\n";
+$NewWallNum=@SingleWallNum[0]*@GroNum[0]*@GroNum[1]*@GroNum[2]*2;
+print "NewWallNum: $NewWallNum\n";
 $match="@MoleName[0]\\s\\+\\([0-9]\\+\\)";
 $replace="@MoleName[0]\\t$NewWallNum";
 #print "$NewWallNum\t$match\t$replace\n";
 system "rm -f tempLeft.gro tempRight.gro LPyOut RPyOut tempRightT.gro output_temp1.gro";
 system "sed 's/$match/$replace/g' $channel_topfile > $OutWallTop";
-
-#The next is to scale given IonFile(gro) to copy the coordinate to channel.
-@IonBoxXYZ=split/\s+/,`tail -1 $IonFile`;
-for ($i=0;$i<3;$i=$i+1){
-    if ($i<2){
-        @IdeaScaleSize[$i]=@NewGroXYZ[$i+1]/@IonBoxXYZ[$i+1];
-    }
-    else{
-        @IdeaScaleSize[$i]=$ChannelLong/@IonBoxXYZ[$i+1];
-    }
-   # print "@IdeaScaleSize[$i] \n";
-}
-$NewGroIonV=@NewGroXYZ[1]*@NewGroXYZ[2]*($ChannelLong-0.2); #0.2 means the surface 0.1 nm each side don't have ion
-$IonV=@IonBoxXYZ[1]*@IonBoxXYZ[2]*@IonBoxXYZ[3];
-@IonPairNum=split/\s+/,`tail -1 $IonTop`;
-system "perl tune_bulk.sh $NewGroIonV $IonV $Kmax $Kmin @IonPairNum[1] > Tune_Out";
-$DeletPairNum=int(`cat Tune_Out`);
-#print "$NewIonPair \n";
-system "rm Tune_Out";
-if($DeletPairNum!=0){
-     $tune_gro="tune.gro";
-     $tune_top="tune.top";
-     system "PYTHON ~/bin/DelMole.py -i $IonFile -F $FirstAtomNum -S $SecondAtomNum -n @IonPairNum[1] -d $DeletPairNum -o $tune_gro";
-     $temp=@IonPairNum[1]-$DeletPairNum;
-     system "cp $IonTop $tune_top";
-     system "sed -i 's/@IonPairNum[1]/$temp/g' $tune_top";
-     system "mkdir tune_bulk; cp *.itp tune_npt.mdp tune_bulk/; mv $tune_gro $tune_top tune_bulk/";
-     system "cd tune_bulk/; grompp -f tune_npt.mdp -c $tune_gro -p $tune_top -o tune.tpr; mdrun -s tune.tpr -v -deffnm tune_end -pin on";
-     system "mv tune_bulk/tune_end.gro ./;cp tune_bulk/$tune_top ./tune_end.top; rm -rf tune_bulk";
-}
 #
-system "perl scale_bulk.sh $channel_grofile tune_end.gro $ChannelLong tune_end.top $FirstAtomNum $SecondAtomNum";
-##above output: IonChannel.gro and IonChannel.top
-#The Next is to copy the channel ion to electrode gro
-@IonChannelXYZ=split/\s+/,`tail -1 IonChannel.gro`;
-@IonTrans[0]=(@NewGroXYZ[1]-@IonChannelXYZ[1])/2;
-@IonTrans[1]=(@NewGroXYZ[2]-@IonChannelXYZ[2])/2;
-@IonTrans[2]=$TotalZLong/2-$ChannelLong/2+($ChannelLong-@IonChannelXYZ[3])/2;
-system "editconf -f IonChannel.gro -translate @IonTrans[0] @IonTrans[1] @IonTrans[2] -o IonTemp.gro";
-system "PYTHON ~/bin/CombineGro.py -l $channel_grofile -r IonTemp.gro -n EminTf2N_Ti3C2 -x @NewGroXYZ[1] -y @NewGroXYZ[2] -z @NewGroXYZ[3] -o $OutPutChannel";
-system "rm -f IonTemp.gro";
-#print "@IonTrans[2] \n";
-system "rm -f IonChannel.gro $channel_grofile";
+##The next is to scale given IonFile(gro) to copy the coordinate to channel.
+#@IonBoxXYZ=split/\s+/,`tail -1 $IonFile`;
+#for ($i=0;$i<3;$i=$i+1){
+#    if ($i<2){
+#        @IdeaScaleSize[$i]=@NewGroXYZ[$i+1]/@IonBoxXYZ[$i+1];
+#    }
+#    else{
+#        @IdeaScaleSize[$i]=$ChannelLong/@IonBoxXYZ[$i+1];
+#    }
+#   # print "@IdeaScaleSize[$i] \n";
+#}
+#$NewGroIonV=@NewGroXYZ[1]*@NewGroXYZ[2]*($ChannelLong-0.2); #0.2 means the surface 0.1 nm each side don't have ion
+#$IonV=@IonBoxXYZ[1]*@IonBoxXYZ[2]*@IonBoxXYZ[3];
+#@IonPairNum=split/\s+/,`tail -1 $IonTop`;
+#system "perl tune_bulk.sh $NewGroIonV $IonV $Kmax $Kmin @IonPairNum[1] > Tune_Out";
+#$DeletPairNum=int(`cat Tune_Out`);
+##print "$NewIonPair \n";
+#system "rm Tune_Out";
+#if($DeletPairNum!=0){
+#     $tune_gro="tune.gro";
+#     $tune_top="tune.top";
+#     system "/opt/python/bin/python DelMole.py -i $IonFile -F $FirstAtomNum -S $SecondAtomNum -n @IonPairNum[1] -d $DeletPairNum -o $tune_gro";
+#     $temp=@IonPairNum[1]-$DeletPairNum;
+#     system "cp $IonTop $tune_top";
+#     system "sed -i 's/@IonPairNum[1]/$temp/g' $tune_top";
+#     system "mkdir tune_bulk; cp *.itp tune_npt.mdp tune_bulk/; mv $tune_gro $tune_top tune_bulk/";
+#     system "cd tune_bulk/; grompp -f tune_npt.mdp -c $tune_gro -p $tune_top -o tune.tpr; mdrun -s tune.tpr -v -deffnm tune_end -pin on";
+#     system "mv tune_bulk/tune_end.gro ./;cp tune_bulk/$tune_top ./tune_end.top; rm -rf tune_bulk";
+#}
+##
+#system "perl scale_bulk.sh $channel_grofile tune_end.gro $ChannelLong tune_end.top $FirstAtomNum $SecondAtomNum";
+###above output: IonChannel.gro and IonChannel.top
+##The Next is to copy the channel ion to electrode gro
+#@IonChannelXYZ=split/\s+/,`tail -1 IonChannel.gro`;
+#@IonTrans[0]=(@NewGroXYZ[1]-@IonChannelXYZ[1])/2;
+#@IonTrans[1]=(@NewGroXYZ[2]-@IonChannelXYZ[2])/2;
+#@IonTrans[2]=$TotalZLong/2-$ChannelLong/2+($ChannelLong-@IonChannelXYZ[3])/2;
+#system "editconf -f IonChannel.gro -translate @IonTrans[0] @IonTrans[1] @IonTrans[2] -o IonTemp.gro";
+#system "/opt/python/bin/python CombineGro.py -l $channel_grofile -r IonTemp.gro -n EminTf2N_Ti3C2 -x @NewGroXYZ[1] -y @NewGroXYZ[2] -z @NewGroXYZ[3] -o $OutPutChannel";
+#system "rm -f IonTemp.gro";
+##print "@IonTrans[2] \n";
+#system "rm -f IonChannel.gro $channel_grofile";
